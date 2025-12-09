a2a376c603f327434ea0a6ff21cfe6e3fa4ad6bc
AdilKhan <asilali864@gmail.com>
2025-12-09 11:23:27 +0530

    added env configuration

--- Skillcase-backend/controllers/notificationController.js
+++ Skillcase-backend/controllers/notificationController.js
@@ -1,6 +1,22 @@
 const admin = require("firebase-admin");
 const { pool } = require("../util/db");
-const serviceAccount = require("../serviceAccountKey.json");
+
+let serviceAccount;
+if (process.env.FIREBASE_PRIVATE_KEY) {
+  serviceAccount = {
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+  };
+} else {
+  // Local development: Use JSON file
+  serviceAccount = require("../serviceAccountKey.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
