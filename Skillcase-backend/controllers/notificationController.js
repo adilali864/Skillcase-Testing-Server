128e14f1adedcddeee9123e2791d3827c33cb4fd
AdilKhan <asilali864@gmail.com>
2025-12-09 11:40:54 +0530

    converted firebase json file to base64

--- Skillcase-backend/controllers/notificationController.js
+++ Skillcase-backend/controllers/notificationController.js
@@ -1,20 +1,12 @@
 const admin = require("firebase-admin");
 const { pool } = require("../util/db");
 
+// Production: use base64 env var, Local: use JSON file
 let serviceAccount;
-if (process.env.FIREBASE_PRIVATE_KEY) {
-  serviceAccount = {
-    type: "service_account",
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-  };
+if (process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
+  const jsonString = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf-8');
+  serviceAccount = JSON.parse(jsonString);
 } else {
-  // Local development: Use JSON file
   serviceAccount = require("../serviceAccountKey.json");
 }
 
